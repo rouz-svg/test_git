45d12a58e137626da13c44b55e7bbe489c874871
rouz-svg <68725083+rouz-svg@users.noreply.github.com>
2020-07-27 11:12:09 +0430

    add index file

---
+++ index.py
@@ -0,0 +1,68 @@
+#call the pakages wee need
+
+import argparse
+import cv2
+import imutils
+
+#define the coordinate of x, y and click is not happened or not correct
+
+noqte=()
+click=False
+
+#define the mouse function and variables
+
+def clik_cut(event,x,y,flags,param):
+
+    global noqte,click
+
+    #if click was left double click perform the commands
+
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+
+        noqte=(x,y)
+        click = True
+
+        #cut the picture and show it
+
+        crop=image[y-50:y+200,x-50:x+200]
+        cv2.imshow("cutt",crop)
+
+        #resize the picture and show it
+
+        resized=imutils.resize(crop,700,700,inter=cv2.INTER_AREA)
+        cv2.imshow("zoom",resized)
+        cv2.waitKey(0)
+
+    #if we press double click right it was false and do not save any coordinates
+
+    elif event == cv2.EVENT_RBUTTONDBLCLK:
+
+        noqte=()
+        click=False
+
+#read the picture and find its path
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i","--image",required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+#create a new window and show the image on it and call hte function clik_cut
+
+cv2.namedWindow("image")
+cv2.setMouseCallback("image",clik_cut)
+
+#out of loop with press key q
+
+while True:
+
+    cv2.imshow("image",image)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+#close all windows
+
+cv2.destroyAllWindows()
